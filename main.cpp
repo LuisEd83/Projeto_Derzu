347c044c3d335459bcbb2665c32f5786bff26e6b
LuisEd83 <eduluis784@gmail.com>
2025-01-15 22:57:43 -0300

    Adicionadas as funções comparador de data e separador de itens  à classe mercadoria

--- main.cpp
+++ main.cpp
@@ -9,21 +9,70 @@ using namespace std;
 using namespace chrono;
 
 template <class T>
-void reinicia_vector(vector<T> nome_vector){
-    for(int i = nome_vector.size(); i>0; i--){
-        nome_vector.erase(nome_vector.end());
+void reinicia_vector(vector<T>& nome_vector){
+    while(!nome_vector.empty()){
+        nome_vector.pop_back();
     }
 }//Basicamente, esta função reinicia um vetor, fazendo ele retornar a ter tamanho 1
+
+typedef struct{
+    int day;
+    int mon;
+    int year;
+}tdata;
 
 class mercadoria{
     public:
         string nome_produto;
         int quantidade;
         float valor;
+        tdata validade;
             mercadoria(){
                 nome_produto = nullptr;
                 quantidade = 0;
                 valor = 0.0;
+                validade = {0,0,0};
+            }
+            bool comparador_data(tdata validade){
+                auto now = system_clock::now();
+                time_t time_now = system_clock::to_time_t(now);
+                tm* local_time = localtime(&time_now);
+                int dia = local_time->tm_mday;
+                int mes = local_time->tm_mon + 1;    
+                int ano = local_time->tm_year + 1900;
+
+                if((validade.day - dia == 0) && (validade.mon - mes == 0) && (validade.year - ano == 0)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+            bool separador(vector<mercadoria>& itens, int separacao, int indice, int quant_x_separacao){
+                
+                if (indice < 0 || indice >= itens.size()) {
+                    cerr << "Índice inválido." << endl;
+                    return false;
+                }
+
+                mercadoria nova_mercadoria;
+                nova_mercadoria.nome_produto = itens[indice].nome_produto + " unidade";
+                nova_mercadoria.quantidade = quant_x_separacao * separacao;
+                nova_mercadoria.valor = (itens[indice].valor / separacao) * 1.2;
+                nova_mercadoria.validade = {itens[indice].validade.day, 
+                                            itens[indice].validade.mon, 
+                                            itens[indice].validade.year};
+
+                for(int i = 0; i<itens.size(); i++){
+                    if(i==indice){
+                        if(itens[i].quantidade - 1 < 0){
+                            return false;
+                        }else{
+                            itens.insert(itens.begin() + indice-1, nova_mercadoria);
+                            return true;
+                        }
+                    }
+                }
+
             }
 };
 
@@ -34,6 +83,7 @@ class cliente{
             cliente(){
                 nome_cliente = nullptr;
                 reinicia_vector(itens);
+                itens[0] = mercadoria();
             }
             int indice(vector<cliente> compradores, string nome_search){
                 int indice = -1;
@@ -52,9 +102,11 @@ class cliente{
 float receita(vector<mercadoria> itens_Co, vector<mercadoria> itens_Ve){
     float venda = 0.0, compra = 0.0;
 
-    for(int i=0; i<itens_Co.size()+itens_Ve.size(); i++){
+    for(int i = 0; i<itens_Co.size(); i++){
         compra += itens_Co[i].quantidade * itens_Co[i].valor;
-        venda += itens_Ve[i].quantidade * itens_Ve[i].valor;
+    }
+    for(int i = 0; i<itens_Ve.size(); i++){
+        venda += itens_Ve[i].quantidade * itens_Ve[i].valor;;
     }
     
     return venda - compra;

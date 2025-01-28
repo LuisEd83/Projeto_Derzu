fe3a6b6029346ca5b177d19ace8029afa1fbd486
LuisEd83 <eduluis784@gmail.com>
2025-01-27 21:30:35 -0300

    Funções atualizadas e mais duas funcionalidades adicionadas ao menu.

--- main.cpp
+++ main.cpp
@@ -682,7 +682,57 @@ void menu(){
                     cout << "Redirecionando ao menu...";
                 }
             break;
-            
+            case 5:
+
+            break;
+            case 6:
+
+            break;
+            case 7:
+
+            break;
+            case 8:
+
+            break;
+            case 9:
+
+            break;
+            case 10:
+
+            break;
+            case 11:
+
+            break;
+            case 12:
+
+            break;
+            case 13:
+
+            break;
+            case 14:
+
+            break;
+            case 15:
+
+            break;
+            case 16:
+
+            break;
+            case 17:
+
+            break;
+            case 18:
+
+            break;
+            case 19:
+
+            break;
+            case 20:
+                cout << "Salvando alterações..." <<endl;
+
+                sleep(2);
+                cout << "Saindo..." <<endl;
+            break;
         }
     }while(alternativa != 20);
 }
